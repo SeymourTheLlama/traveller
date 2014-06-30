358e230aca718e758448282ae51baae07044e447
SeymourTheLlama <andrewmburcham@gmail.com>
2014-06-29 20:58:01 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+var locations = [
+  {
+    type: "lake",
+    possible_names: ["Lake Trasimere", "Mirror Lake", "Lake 'o Bacon"],
+    incompatible_biomes: ["desert", "fungal"]
+  },
+  {
+    type: "city",
+    possible_names: ["Boston", "Taris", "Aes Sedai", "Elvisville"]
+    incompatible_biomes: ["glacier"]
+  },
+];
