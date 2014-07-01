d5d63e518a27d676a63d23d92de3ad656ad47684
SeymourTheLlama <andrewmburcham@gmail.com>
2014-06-30 17:55:34 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,16 @@
+var biomes = [
+  {
+    type: "desert",
+    possible_names: ["the Sahara", "the Hot Place"]
+  },
+  {
+    type: "plains"
+  },
+  {
+    type: "hilly"
+  }
+];
+
 var locations = [
   {
     type: "lake",
@@ -6,7 +19,17 @@ var locations = [
   },
   {
     type: "city",
-    possible_names: ["Boston", "Taris", "Aes Sedai", "Elvisville"]
+    possible_names: ["Boston", "Taris", "Aes Sedai", "Elvisville"],
     incompatible_biomes: ["glacier"]
   },
+  {
+    type: "river",
+    possible_names: ["Anduin", "Swiggle River"],
+    incompatible_biomes: ["glacier", "desert"]
+  },
+  {
+    type: "capital_city",
+    possible_names: ["name_of_nation City", "Bacon City", "Smile City"],
+    compatible_biomes: ["hilly", "plains", "grasslands"]
+  }
 ];
